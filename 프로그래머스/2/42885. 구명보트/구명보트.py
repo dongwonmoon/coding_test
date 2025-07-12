9bb3705c3fc4df76126fe1ef9be3330bd9d0154b
DongWon <ansehddnjs187@gmail.com>
2025-07-12 19:24:06 +0900

    [level 2] Title: 구명보트, Time: 9.71 ms, Memory: 9.56 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,11 @@
+def solution(people, limit):
+    people.sort()
+    i, j = 0, len(people) - 1
+    count = 0
+
+    while i <= j:
+        if people[i] + people[j] <= limit:
+            i += 1
+        j -= 1
+        count += 1
+    return count

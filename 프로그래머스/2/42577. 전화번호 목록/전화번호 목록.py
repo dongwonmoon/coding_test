710d65919934f062c0729ff8e513451232dfda87
DongWon <ansehddnjs187@gmail.com>
2025-07-26 23:59:26 +0900

    [level 2] Title: 전화번호 목록, Time: 459.91 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,14 +1,14 @@
-from collections import defaultdict
-
 def solution(phone_book):
-    hash_set = set()
-    
-    for p in phone_book:
-        for i in range(1, len(p)):
-            hash_set.add(p[:i])
-    
-    for p in phone_book:
-        if p in hash_set:
-            return False
-        
+    # 모든 전화번호를 해시셋에 저장
+    phone_set = set(phone_book)
+
+    # 각 번호에 대해, 길이가 1부터 (len-1)까지의 접두사만 해시셋에 있는지 확인
+    for number in phone_book:
+        # 접두사 길이 i: 1, 2, ..., len(number)-1
+        for i in range(1, len(number)):
+            if number[:i] in phone_set:
+                # 어떤 다른 번호가 현재 번호의 접두사라면 False
+                return False
+
+    # 모든 검사 통과 시 True
     return True

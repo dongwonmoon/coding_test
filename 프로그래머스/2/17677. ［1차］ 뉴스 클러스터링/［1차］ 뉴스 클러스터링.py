23c070fccdf0d9e8323ef3aab46436641a4c2a8a
DongWon <ansehddnjs187@gmail.com>
2025-08-02 23:20:34 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.87 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,36 +1,11 @@
-from collections import Counter, defaultdict
-
-def make_list(s):
-    l = []
-    for i in range(len(s)):
-        w = s[i:i+2]
-        if len(w) == 2 and w.isalpha():
-            l.append(w)
-    return l
-
+from collections import Counter
 def solution(str1, str2):
-    str1, str2 = str1.lower(), str2.lower()
-    s1 = make_list(str1)
-    s2 = make_list(str2)
-    
-    if s1 == s2 and s1 == []:
+    s1 = [str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
+    s2 = [str2[i:i+2].lower() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
+    if not s1 and not s2:
         return 65536
-    
-    s1 = Counter(s1)
-    s2 = Counter(s2)
-    
-    a1 = s1 if len(s1) < len(s2) else s2
-    a2 = s1 if a1 == s2 else s2
-    
-    inter = defaultdict(int)
-    uni = defaultdict(int)
-    for s in a1:
-        if s in a2:
-            inter[s] = a1[s] if a1[s] < a2[s] else a2[s]
-        uni[s] = a1[s]
-    for s in a2:
-        uni[s] = a2[s]
-        if s in a1:
-            uni[s] = a2[s] if a2[s] > a1[s] else a1[s]
-            
-    return 65536 * sum(inter.values()) // sum(uni.values())
+    c1 = Counter(s1)
+    c2 = Counter(s2)
+    print(c1&c2)
+    answer = int(float(sum((c1&c2).values()))/float(sum((c1|c2).values())) * 65536)
+    return answer

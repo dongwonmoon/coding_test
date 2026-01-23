032f7761739f97c4917bdf481223da76bf8d2291
DongWon <ansehddnjs187@gmail.com>
2026-01-23 15:12:58 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2812 ms, Memory: 56024 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+points = []
+for _ in range(N):
+    x, y = map(int, input().split())
+    points.append([x, y])
+
+sorted_points = sorted(points, key=lambda point: (point[1], point[0]))
+
+for point in sorted_points:
+    print(point[0], point[1])

01fa7ff2064568ebd3cae8839a2283d514579bac
DongWon <ansehddnjs187@gmail.com>
2026-02-06 21:15:24 +0900

    [Silver V] Title: 다리 놓기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,11 +1,4 @@
 import sys
-from functools import lru_cache
-
-
-@lru_cache
-def fact(num):
-    return num * fact(num - 1) if num > 1 else 1
-
 
 input = sys.stdin.readline
 
@@ -13,4 +6,10 @@ T = int(input())
 
 for _ in range(T):
     N, M = map(int, input().split())
-    print(int(fact(M) / (fact(N) * fact(M - N))))
+    table = {(1, j): 1 for j in range(1, M + 1)}
+
+    for i in range(2, N + 1):
+        for j in range(i, M - N + i + 1):
+            table[i, j] = table.get((i - 1, j - 1), 0) + table.get((i, j - 1), 0)
+
+    print(sum([table[N, j] for j in range(N, M + 1)]))

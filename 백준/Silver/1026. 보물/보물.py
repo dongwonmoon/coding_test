212aa178fdca44614760869e144d57accc3d81e9
DongWon <ansehddnjs187@gmail.com>
2026-02-08 01:30:05 +0900

    [Silver IV] Title: 보물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+input()
+
+A = sorted(list(map(int, input().split())))
+
+B = sorted(list(map(int, input().split())), reverse=True)
+
+print(sum([a * b for a, b in zip(A, B)]))

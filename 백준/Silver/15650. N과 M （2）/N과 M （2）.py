9ff9d7b4ed2e2d13f8bc4d6ac975e16179199193
DongWon <ansehddnjs187@gmail.com>
2026-02-11 19:35:42 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,24 +1,15 @@
 N, M = map(int, input().split())
 
-nums = list(range(1, N + 1))
-visited = [0] * N
-s = set()
 
-
-def build(path, state):
-    p = tuple(sorted(path))
-    if len(path) == M and p not in s:
-        s.add(p)
+def build(path, n):
+    if len(path) == M:
         print(*path)
         return
 
-    for i in range(N):
-        if state[i] == 0:
-            state[i] = 1
-            path.append(nums[i])
-            build(path, state)
-            path.pop()
-            state[i] = 0
+    for i in range(n, N + 1):
+        path.append(i)
+        build(path, i + 1)
+        path.pop()
 
 
-build([], visited)
+build([], 1)

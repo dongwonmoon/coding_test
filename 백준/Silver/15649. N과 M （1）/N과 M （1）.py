dd5a0309c88120d22c22320820bf8cd5a4c9c8a1
DongWon <ansehddnjs187@gmail.com>
2026-02-11 13:17:21 +0900

    [Silver III] Title: N과 M (1), Time: 112 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,5 +1,23 @@
-from itertools import permutations
+import sys
 
 N, M = map(int, input().split())
-for t in sorted(list(permutations(range(1, N + 1), M))):
-    print(*t)
+
+nums = list(range(1, N + 1))
+visited = [0] * N
+
+
+def build(path, state):
+    if len(path) == M:
+        sys.stdout.write(" ".join(map(str, path)) + "\n")
+        return
+
+    for i in range(N):
+        if state[i] == 0:
+            state[i] = 1
+            path.append(nums[i])
+            build(path, state)
+            path.pop()
+            state[i] = 0
+
+
+build([], visited)

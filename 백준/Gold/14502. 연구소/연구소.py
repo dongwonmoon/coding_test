5d1feb12194660e9725c5d99e188fcf4097d3dde
DongWon <ansehddnjs187@gmail.com>
2026-03-16 16:01:32 +0900

    [Gold IV] Title: 연구소, Time: 3576 ms, Memory: 36060 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,6 +1,5 @@
 import sys
 from itertools import combinations
-from copy import deepcopy
 from collections import deque
 
 input = sys.stdin.readline
@@ -8,48 +7,48 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 
 board = []
-safe = []
+empty = []
+virus = []
 
-for i in range(N):
+for y in range(N):
     row = list(map(int, input().split()))
-
-    for j, item in enumerate(row):
-        if item == 0:
-            safe.append((i, j))
-
     board.append(row)
 
-wall_poss = combinations(safe, 3)
+    for x, value in enumerate(row):
+        if value == 0:
+            empty.append((y, x))
+        elif value == 2:
+            virus.append((y, x))
 
-q_og = deque()
-for y in range(N):
-    for x in range(M):
-        if board[y][x] == 2:
-            q_og.append((y, x))
-
-safe_count = 0
+empty_count = len(empty)
+answer = 0
 dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-for coords in wall_poss:
-    temp_board = deepcopy(board)
-    for coord in coords:
-        i, j = coord
-        temp_board[i][j] = 1
+for walls in combinations(empty, 3):
+    for y, x in walls:
+        board[y][x] = 1
 
-    q = q_og.copy()
+    q = deque(virus)
+    infected = []
+    infected_count = 0
+
     while q:
         y, x = q.popleft()
-        for dx, dy in dirs:
+
+        for dy, dx in dirs:
             ny, nx = y + dy, x + dx
-            if 0 <= ny < N and 0 <= nx < M and temp_board[ny][nx] == 0:
-                temp_board[ny][nx] = 2
+            if 0 <= ny < N and 0 <= nx < M and board[ny][nx] == 0:
+                board[ny][nx] = 2
+                infected.append((ny, nx))
+                infected_count += 1
                 q.append((ny, nx))
 
-    count = 0
-    for row in temp_board:
-        count += row.count(0)
+    safe_area = empty_count - 3 - infected_count
+    answer = max(answer, safe_area)
 
-    if safe_count < count:
-        safe_count = count
+    for y, x in infected:
+        board[y][x] = 0
+    for y, x in walls:
+        board[y][x] = 0
 
-print(safe_count)
+print(answer)
